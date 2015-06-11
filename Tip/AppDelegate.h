b06488d5a5c97d4d01a4f99e5b77e92db1d10054
Jason Huang <cjhuang@yahoo-inc.com>
2015-06-12 00:51:53 +0800

    init project, setup storyboard

---
+++ Tip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip
+//
+//  Created by Jason Huang on 6/12/15.
+//  Copyright (c) 2015 Jason Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
